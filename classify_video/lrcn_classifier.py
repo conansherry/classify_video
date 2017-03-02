b6e88ee9ffcfba1e349db5c2806b3e8b560c67d5
conansherry <conansherry@163.com>
2017-03-02 14:50:05 +0800

    update

--- classify_video/lrcn_classifier.py
+++ classify_video/lrcn_classifier.py
@@ -7,7 +7,7 @@ def lrcn_classifier(video_dict, net):
     clip_length = 16
     key_list = []
     caffe_in = np.empty((0, 3, 227, 227))
-    clip_markers = np.empty((0, 1, 1, 1))
+    clip_markers_in = np.empty((0, 1, 1, 1))
     output_predictions = np.zeros((len(video_dict), 2))
 
     for key in video_dict:
@@ -23,11 +23,24 @@ def lrcn_classifier(video_dict, net):
 
         clip_clip_markers = np.ones((clip_length, 1, 1, 1))
         clip_clip_markers[0, :, :, :] = 0
-        clip_markers = np.concatenate((clip_markers, clip_clip_markers), axis=0)
+        clip_markers_in = np.concatenate((clip_markers_in, clip_clip_markers), axis=0)
 
-    out = net.forward_all(data=caffe_in, clip_markers=np.array(clip_markers))
-    print out
-    output_predictions = np.mean(out['probs'], 1)
-    print 'out[\'probs\'].shape', out['probs'].shape
-    print 'output_predictions.shape', output_predictions.shape
-    print np.mean(output_predictions, 0).argmax(), output_predictions
+    print caffe_in.shape
+    print clip_markers_in.shape
+    net.blobs['data'].reshape(*(caffe_in.shape))
+    net.blobs['clip_markers'].reshape(*(clip_markers_in.shape))
+    out = net.forward_all(data=caffe_in, clip_markers=clip_markers_in)
+    for blob in net.blobs:
+        print blob, net.blobs[blob].data[...].shape
+    for input_blob in net.inputs:
+        print input_blob
+    print out['probs'].shape
+    print np.mean(out['probs'], 0).shape
+    print np.argmax(np.mean(out['probs'], 0), axis=1)
+    res_label = np.argmax(np.mean(out['probs'], 0), axis=1)
+    output_res = {}
+    index = 0
+    for key in key_list:
+        output_res[key] = res_label[index]
+        index = index + 1
+    print output_res
