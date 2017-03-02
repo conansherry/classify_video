a08cf1982490f86405ff8483d30da385f621d3a1
conansherry <conansherry@163.com>
2017-03-02 14:52:29 +0800

    update.

--- classify_video/lrcn_classifier.py
+++ classify_video/lrcn_classifier.py
@@ -25,18 +25,18 @@ def lrcn_classifier(video_dict, net):
         clip_clip_markers[0, :, :, :] = 0
         clip_markers_in = np.concatenate((clip_markers_in, clip_clip_markers), axis=0)
 
-    print caffe_in.shape
-    print clip_markers_in.shape
+    #print caffe_in.shape
+    #print clip_markers_in.shape
     net.blobs['data'].reshape(*(caffe_in.shape))
     net.blobs['clip_markers'].reshape(*(clip_markers_in.shape))
     out = net.forward_all(data=caffe_in, clip_markers=clip_markers_in)
-    for blob in net.blobs:
-        print blob, net.blobs[blob].data[...].shape
-    for input_blob in net.inputs:
-        print input_blob
-    print out['probs'].shape
-    print np.mean(out['probs'], 0).shape
-    print np.argmax(np.mean(out['probs'], 0), axis=1)
+    # for blob in net.blobs:
+    #     print blob, net.blobs[blob].data[...].shape
+    # for input_blob in net.inputs:
+    #     print input_blob
+    # print out['probs'].shape
+    # print np.mean(out['probs'], 0).shape
+    # print np.argmax(np.mean(out['probs'], 0), axis=1)
     res_label = np.argmax(np.mean(out['probs'], 0), axis=1)
     output_res = {}
     index = 0
